3a5becf9ab7d8f7ff2f3e732662b827c643acdcf
Chanut <117045982+Chanut-NN@users.noreply.github.com>
2026-07-22 00:29:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js - ไฟล์ตั้งค่าแยกเพื่อความปลอดภัยและง่ายต่อการจัดการ
 const CONFIG = {
   // URL ของ Google Apps Script Web App
-  DEFAULT_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbzmwuDHeZiUnJuaQ9qTZVr34tftCpxb4owkKi_2ObgI1CqHaAubipxEgdHNBNCYEGa-6w/exec',
+  DEFAULT_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbwNG4dGTxmMso0FNaSw10bBvM0a3BVmrwdXrHE5pkWDzJhP2_nFDVM6jBDh8NdrnFP8_Q/exec',
   
   // รหัสผ่านสำหรับเข้าแก้ไข URL
   DEV_PASSCODE: 'DLDAQS@2569'
