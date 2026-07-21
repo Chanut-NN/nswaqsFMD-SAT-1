f90d8bead13e7ef98e1d669b9038c8a7b3dec9b8
Chanut <117045982+Chanut-NN@users.noreply.github.com>
2026-07-22 03:18:57 +0700

    Change web app URLs in config.js
    Updated web app URLs in the configuration.

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   // นำ Web App URL ที่ได้จาก Google Apps Script มาวางตรงนี้
-  DEFAULT_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbzCNgQdaLV6nKi7AgV_URsRJ022h78dag-1tw4ZZgxncTaqkX6mN5ukvI-FI2P6RmSX/exec',
-  WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbzCNgQdaLV6nKi7AgV_URsRJ022h78dag-1tw4ZZgxncTaqkX6mN5ukvI-FI2P6RmSX/exec',
+  DEFAULT_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbxZ7V5TSWmJCh53GfShhYYoClMGYj_tKgQvjELSaPfqfxjNb8x4YWDmRpApHyZp2C9a/exec',
+  WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbxZ7V5TSWmJCh53GfShhYYoClMGYj_tKgQvjELSaPfqfxjNb8x4YWDmRpApHyZp2C9a/exec',
   DEV_PASSCODE: 'DLDAQS@2569'
 };
