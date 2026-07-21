00ce418addfeaff56c71ad0a9198f0e719689b93
Chanut <117045982+Chanut-NN@users.noreply.github.com>
2026-07-21 23:16:59 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+// config.js - ไฟล์ตั้งค่าแยกเพื่อความปลอดภัยและง่ายต่อการจัดการ
+const CONFIG = {
+  // URL ของ Google Apps Script Web App
+  DEFAULT_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbyBM9eBIW-EExX7SdqhQkGi4VsYUXw1tlAe7jPIK0IOCAOIbuC9EF6XFR931bwN673K/exec',
+  
+  // รหัสผ่านสำหรับเข้าแก้ไข URL
+  DEV_PASSCODE: 'DLDAQS@2569'
+};
